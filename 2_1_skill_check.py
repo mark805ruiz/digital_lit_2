305413e956d8bb339eae094aebba453941af3ab5
mark805ruiz <77691714+mark805ruiz@users.noreply.github.com>
2022-04-15 13:10:52 -0600

    Add files via upload

---
+++ 2_1_skill_check.py
@@ -0,0 +1,7 @@
+import random
+num_1 =  random.randint(1, 50)
+num_2 = random.randint(1,50)
+
+num_mult = num_1*num_2
+print('We are multiplying ' + str(num_1) + ' by ' + str(num_2))
+print(num_mult)
---
+++ guess_2_1.py
@@ -0,0 +1,6 @@
+import random
+print('Hello! Select a number between 1 and 20')
+myNumber = input()
+number = random.randint(1, 20)
+print('The number you selected is ' + myNumber)
+print(int(myNumber))
