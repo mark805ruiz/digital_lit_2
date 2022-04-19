aa84d78167e4371bc93be7387fc790fbae2a6bd1
mark805ruiz <77691714+mark805ruiz@users.noreply.github.com>
2022-04-18 18:44:51 -0600

    Add files via upload

---
+++ lessons/testing_2.py
@@ -0,0 +1,4 @@
+print('"When you are courting a nice girl [or boy] an hour seems like a second.')
+print('When you sit on a red-hot cinder a second seems like an hour.')
+print('That\'s relativity."')
+print('-Albert Einstein')
