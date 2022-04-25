d1296d8dd16b7dcc95bc1466980724925ba1750b
mark805ruiz <77691714+mark805ruiz@users.noreply.github.com>
2022-04-24 22:49:39 -0600

    Add files via upload

---
+++ lessons/return_testing.py
@@ -0,0 +1,29 @@
+def calculate_average(a, b):
+    """ Calculate an average of two numbers """
+    result = (a + b) / 2
+    return result
+ 
+# Pretend you have some code here
+x = 45
+y = 56
+ 
+average = calculate_average(x, y)
+print(average)
+
+def square(n):
+  """Returns the square of a number."""
+  squared = n ** 2
+  print ("%d squared is %d." % (n, squared))
+  return squared
+  
+# Call the square function on line 10! Make sure to
+# include the number 10 between the parentheses.
+
+square(10)
+
+
+def power(base,exponent):  # Add your parameters here!
+  result = base ** exponent
+  print ("%d to the power of %d is %d." % (base, exponent, result))
+
+power(37,4)  # Add your arguments here!
