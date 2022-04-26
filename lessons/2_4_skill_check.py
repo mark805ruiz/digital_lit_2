41ed653d9840aee5221552f6a20d5e0898c1ca90
mark805ruiz <77691714+mark805ruiz@users.noreply.github.com>
2022-04-25 23:05:31 -0600

    Add files via upload

---
+++ lessons/2_4_skill_check.py
@@ -0,0 +1,12 @@
+#def hello(name):        
+ #       print('Hello ' + name + '. It is a pleasure to meet you')
+#hello('Jim')
+#hello('Hazel')
+#hello('fish')
+#hello('season')
+
+#OR we can do this:
+def hello(name):
+        print('Hello %s. It is a pleasure to meet you.' % (name))
+        return name
+hello('jay')        
